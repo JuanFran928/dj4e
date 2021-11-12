8ea63563b55e025934a79a8c3f3395736a407fb0
Juan Fran Martin <juanframaro@gmail.com>
2021-11-12 20:03:15 +0000

    removed unused libraries

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
 import os
-from . import views
 from django.contrib.auth import views as auth_views
 from django.contrib import admin
 from django.urls import include, path
-from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls import url
 from django.conf.urls.static import static
